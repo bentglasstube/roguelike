552c5e3fb7f2b7883d060bd899980bec72f19747
Alan Berndt <alan@eatabrick.org>
2021-05-07 20:01:57 -0700

    Update config and main for web.

--- config.cc
+++ config.cc
@@ -4,4 +4,6 @@ Config::Config() : Game::Config() {
   graphics.title = "Roguelike";
   graphics.width = 256;
   graphics.height = 240;
+  graphics.intscale = 3;
+  graphics.fullscreen = false;
 }
--- main.cc
+++ main.cc
@@ -3,9 +3,24 @@
 #include "config.h"
 #include "title_screen.h"
 
+#ifdef __EMSCRIPTEN__
+#include "emscripten.h"
+
+void step(void* game) {
+  static_cast<Game*>(game)->step();
+}
+#endif
+
 int main(int, char**) {
   Game game(kConfig);
-  game.loop(new TitleScreen());
+  Screen *start = new TitleScreen();
+
+#ifdef __EMSCRIPTEN__
+  game.start(start);
+  emscripten_set_main_loop_arg(step, &game, 0, true);
+#else
+  game.loop(start);
+#endif
 
   return 0;
 }
